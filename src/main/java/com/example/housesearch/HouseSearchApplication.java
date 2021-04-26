e6f53168884e5cdf8b145ce9389f828af633274c
lastFeng <563805728@qq.com>
2021-04-26 21:14:55 +0800

    增加初始化依赖

---
+++ src/main/java/com/example/housesearch/HouseSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.housesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseSearchApplication.class, args);
+    }
+
+}
