e1985806431b9adfa7fedfac0aa427ccdc2aa583
lastFeng <563805728@qq.com>
2021-04-27 13:35:07 +0800

    增加es启动的错误，将Processors属性配置为false

--- src/main/java/com/example/housesearch/HouseSearchApplication.java
+++ src/main/java/com/example/housesearch/HouseSearchApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HouseSearchApplication {
 
     public static void main(String[] args) {
+        System.setProperty("es.set.netty.runtime.available.processors", "false");
         SpringApplication.run(HouseSearchApplication.class, args);
     }
 
